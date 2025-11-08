84221bd068ce6074e4a89050c3f83b33d4f648a1
Kuy Tangkoan <kuytangkoan@gmail.com>
2025-11-08 11:35:38 +0700

    first commit

---
+++ application/extra/site.php
@@ -0,0 +1,42 @@
+<?php
+
+return array (
+  'name' => 'Nealika Demo',
+  'beian' => '',
+  'cdnurl' => '',
+  'version' => '1.0.2',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'en',
+    'frontend' => 'en',
+  ),
+  'fixedpage' => 'dashboard',
+  'categorytype' => 
+  array (
+    'default' => 'default',
+    'page' => 'Page',
+    'article' => 'Article',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => 'Basic',
+    'email' => 'Mail',
+    'dictionary' => 'Dictionary',
+    'user' => 'Members',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '',
+  'mail_smtp_pass' => '',
+  'mail_verify_type' => '2',
+  'mail_from' => '',
+  'attachmentcategory' => 
+  array (
+    'category1' => 'Category 1',
+    'category2' => 'Category 2',
+    'custom' => 'customize',
+  ),
+);
